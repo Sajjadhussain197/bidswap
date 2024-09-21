500f0696263b4bfb23690d2ee2a1b91d145fcc85
sajjadhussain197 <sajjadhussain9582@gmail.com>
2024-09-21 10:30:58 +0500

    added

--- app/(backend)/dashboard/orders/[id]/invoice/page.jsx
+++ app/(backend)/dashboard/orders/[id]/invoice/page.jsx
@@ -1,10 +1,11 @@
-// import SalesInvoice from "@/components/Order/SalesInvoice";
 import SalesInvoice from "@/app/components/order/salesInvoice";
 import { getData } from "@/lib/getData";
 import React from "react";
 
-export default async function page({params:{id}}) {
-    const order =await  getData(`orders/${id}`);
-   // console.log(order);
-      <SalesInvoice  order ={order}/>
+export default async function Page({ params: { id } }) {
+  const order = await getData(`orders/${id}`);
+  
+  return (
+    <SalesInvoice order={order} />
+  );
 }
